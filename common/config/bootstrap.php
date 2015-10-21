47064a6898b193f287fb0c9640f46217cf1e8c57
Tung Mang Vien <tungmv7@gmail.com>
2015-10-21 18:44:08 +0700

    update source

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,4 +4,5 @@ Yii::setAlias('frontend', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'fro
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'console');
 Yii::setAlias('eff', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'eff');
+Yii::setAlias('files', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'files');
 Yii::setAlias('marketplace', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'marketplace');
